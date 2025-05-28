fd7b6e0bd1ca69ec91d36648c37e830fe1c4d9e9
TiboP33 <144940262+TiboP33@users.noreply.github.com>
2025-05-28 12:37:50 +0000

    test

--- test.py
+++ test.py
@@ -1,46 +1,63 @@
-def galois_multiplication(a, b):
-    result = 0
-    for _ in range(8):
-        if b & 1:
-            result ^= a
-        hi_bit_set = a & 0x80
-        a <<= 1
-        if hi_bit_set:
-            a ^= 0x1b
-        a &= 0xFF
-        b >>= 1
-    return result
+from copy import deepcopy
 
+mix_column_matrix = [[0x02,0x03,0x01,0x01],
+                     [0x01,0x02,0x03,0x01],
+                     [0x01,0x01,0x02,0x03],
+                     [0x03,0x01,0x01,0x02]]
 
-def MixColumns(state):
-    for j in range(4):
-        s0 = int(state[0][j], 16)
-        s1 = int(state[1][j], 16)
-        s2 = int(state[2][j], 16)
-        s3 = int(state[3][j], 16)
+def GaloisMultiplication(number, multiplier):
+   number_bin = format(number, '08b')
+   if multiplier == 0x01:
+      return number
+   elif multiplier == 0x02:
+      mask = 2 ** 8 - 1
+      num_shifted = (number << 1) & mask
+      if number_bin[0] == '0':
+         return num_shifted
+      else:
+         return (num_shifted ^ 0b00011011)
+   elif multiplier == 0x03:
+      return (GaloisMultiplication(number, 0x02) ^ number)
+   elif multiplier == 0x09:
+      a = number
+      for i in range(0, 3):
+         a = GaloisMultiplication(a, 0x02)
+      return (a ^ number)
+   elif multiplier == 0x0b:
+      a = number
+      for i in range(0, 2):
+         a = GaloisMultiplication(a, 0x02)
+      a = a ^ number
+      a = GaloisMultiplication(a, 0x02)
+      return (a ^ number)
+   elif multiplier == 0x0d:
+      a = number
+      a = GaloisMultiplication(a, 0x02)
+      a = a ^ number
+      for i in range(0, 2):
+         a = GaloisMultiplication(a, 0x02)
+      return (a ^ number)
+   elif multiplier == 0x0e:
+      a = number
+      for i in range(0, 2):
+         a = GaloisMultiplication(a, 0x02)
+         a = a ^ number
+      return (GaloisMultiplication(a, 0x02))
 
-        t0 = galois_multiplication(0x02, s0) ^ galois_multiplication(0x03, s1) ^ s2 ^ s3
-        t1 = s0 ^ galois_multiplication(0x02, s1) ^ galois_multiplication(0x03, s2) ^ s3
-        t2 = s0 ^ s1 ^ galois_multiplication(0x02, s2) ^ galois_multiplication(0x03, s3)
-        t3 = galois_multiplication(0x03, s0) ^ s1 ^ s2 ^ galois_multiplication(0x02, s3)
+def MixColumns(matrix):
+   new_matrix = deepcopy(matrix)
+   for c in range(0, 4):
+      new_column = [0,0,0,0]
+      column = [new_matrix[0][c],new_matrix[1][c],new_matrix[2][c],new_matrix[3][c]]
+      for row in range(0, 4):
+         for col in range(0, 4):
+            new_column[row] ^= GaloisMultiplication(int(column[col], 16), mix_column_matrix[row][col])
+      for r in range(0, 4):
+         new_matrix[r][c] = "0x{:02x}".format(new_column[r])
+   return new_matrix
 
-        state[0][j] = format(t0, '02x')
-        state[1][j] = format(t1, '02x')
-        state[2][j] = format(t2, '02x')
-        state[3][j] = format(t3, '02x')
+matrice = [['63', '7b', 'c0', 'd2'], ['7b', '76', 'd2', '7c'], ['76', '75', '7c', 'c5'], ['75', '63', 'c5', 'c0']]
 
-    return state
-
-
-
-matrice = [
-    ['db', '13', '53', '45'],
-    ['f2', '0a', '22', '5c'],
-    ['01', '01', '01', '01'],
-    ['c6', 'c6', 'c6', 'c6']
-]
-attendu = [["59","1c","ee","a1"],["c2","86","36","d1"],["ca","dd","af","02"],["4a","27","dc","a2"]]
-
+attendu = [['0x48', '0x7a', '0x4f', '0x3e'], ['0x7a', '0x6b', '0x3e', '0xbe'], ['0x6b', '0x42', '0xbe', '0x64'], ['0x42', '0x48', '0x64', '0x4f']]
 result = MixColumns(matrice)
 print(result)
-
