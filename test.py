eb3ca0ee6c732de383ba65b5c2ffa1e170e5f7d4
TiboP33 <144940262+TiboP33@users.noreply.github.com>
2025-05-30 17:24:12 +0000

    test ùixcolumns

--- test.py
+++ test.py
@@ -1,63 +1,33 @@
-from copy import deepcopy
+def xtime(x):
+    return ((x << 1) ^ 0x1b) & 0xFF if (x & 0x80) else (x << 1) & 0xFF
 
-mix_column_matrix = [[0x02,0x03,0x01,0x01],
-                     [0x01,0x02,0x03,0x01],
-                     [0x01,0x01,0x02,0x03],
-                     [0x03,0x01,0x01,0x02]]
+def mix_columns(state):
+    for i in range(4):
+        idx = i * 4
+        a = state[idx]
+        b = state[idx + 1]
+        c = state[idx + 2]
+        d = state[idx + 3]
 
-def GaloisMultiplication(number, multiplier):
-   number_bin = format(number, '08b')
-   if multiplier == 0x01:
-      return number
-   elif multiplier == 0x02:
-      mask = 2 ** 8 - 1
-      num_shifted = (number << 1) & mask
-      if number_bin[0] == '0':
-         return num_shifted
-      else:
-         return (num_shifted ^ 0b00011011)
-   elif multiplier == 0x03:
-      return (GaloisMultiplication(number, 0x02) ^ number)
-   elif multiplier == 0x09:
-      a = number
-      for i in range(0, 3):
-         a = GaloisMultiplication(a, 0x02)
-      return (a ^ number)
-   elif multiplier == 0x0b:
-      a = number
-      for i in range(0, 2):
-         a = GaloisMultiplication(a, 0x02)
-      a = a ^ number
-      a = GaloisMultiplication(a, 0x02)
-      return (a ^ number)
-   elif multiplier == 0x0d:
-      a = number
-      a = GaloisMultiplication(a, 0x02)
-      a = a ^ number
-      for i in range(0, 2):
-         a = GaloisMultiplication(a, 0x02)
-      return (a ^ number)
-   elif multiplier == 0x0e:
-      a = number
-      for i in range(0, 2):
-         a = GaloisMultiplication(a, 0x02)
-         a = a ^ number
-      return (GaloisMultiplication(a, 0x02))
+        state[idx]     = xtime(a) ^ xtime(b) ^ b ^ c ^ d
+        state[idx + 1] = a ^ xtime(b) ^ xtime(c) ^ c ^ d
+        state[idx + 2] = a ^ b ^ xtime(c) ^ xtime(d) ^ d
+        state[idx + 3] = xtime(a) ^ a ^ b ^ c ^ xtime(d)
+    return state
 
-def MixColumns(matrix):
-   new_matrix = deepcopy(matrix)
-   for c in range(0, 4):
-      new_column = [0,0,0,0]
-      column = [new_matrix[0][c],new_matrix[1][c],new_matrix[2][c],new_matrix[3][c]]
-      for row in range(0, 4):
-         for col in range(0, 4):
-            new_column[row] ^= GaloisMultiplication(int(column[col], 16), mix_column_matrix[row][col])
-      for r in range(0, 4):
-         new_matrix[r][c] = "0x{:02x}".format(new_column[r])
-   return new_matrix
+# Entrée hexadécimale sous forme de matrice
+matrice = [
+    ['63', '7b', 'c0', 'd2'],
+    ['7b', '76', 'd2', '7c'],
+    ['76', '75', '7c', 'c5'],
+    ['75', '63', 'c5', 'c0']
+]
 
-matrice = [['63', '7b', 'c0', 'd2'], ['7b', '76', 'd2', '7c'], ['76', '75', '7c', 'c5'], ['75', '63', 'c5', 'c0']]
+# Aplatir la matrice et convertir en entiers
+flat_state = [int(cell, 16) for row in matrice for cell in row]
 
-attendu = [['0x48', '0x7a', '0x4f', '0x3e'], ['0x7a', '0x6b', '0x3e', '0xbe'], ['0x6b', '0x42', '0xbe', '0x64'], ['0x42', '0x48', '0x64', '0x4f']]
-result = MixColumns(matrice)
-print(result)
+# Appliquer MixColumns
+result = mix_columns(flat_state)
+
+# Afficher le résultat en hexadécimal
+print(''.join(f'{x:02x}' for x in result))
